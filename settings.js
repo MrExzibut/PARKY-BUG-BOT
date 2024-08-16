a535594408709bc5420345f1b82b19a4e6110863
MrExzibut <ndbruno7@gmail.com>
2024-08-16 23:42:28 +0100

    Update settings.js

--- settings.js
+++ settings.js
@@ -11,15 +11,15 @@ const fs = require('fs')
 const chalk = require('chalk')
 
 //contact details
-global.ownernomer = "22898133388"
-global.ownername = "🩵 𝙹𝚎𝚊𝚗 𝙿𝚊𝚛𝚔𝚎𝚛"
+global.ownernomer = "237695466413"
+global.ownername = "🩵 Exzibut king"
 global.ytname = "YT: Jean-parker-tech"
 global.socialm = "GitHub: Jeanparker100"
 global.location = "Togo, maritime, Lomé"
 
 global.ownernumber = '22898133388'  //creator number
 global.ownername = '𝙹𝚎𝚊𝚗 𝙿𝚊𝚛𝚔𝚎𝚛' //owner name
-global.botname = '𝙿𝙰𝚁𝙺𝚈-𝙱𝚄𝙶-𝙱𝙾𝚃' //name of the bot
+global.botname = '♡●♰亗『мᏒ』ꂅꊼᏃᎥცŭ̈Ϯ〖ᴷⁱⁿᴳ◦•●' //name of the bot
 
 //sticker details
 global.packname = '¹⁴³ ᴬˡᵉᵐᵃᵖ 🧸💜'
